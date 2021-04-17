e50779610ffcac9a9e170df9e4f3050db19caec8
Mark Gross <mark.gross2001@gmail.com>
2021-04-17 11:20:57 -0700

    Loop and remove readings Vec
    There's no reason to collect all the readings into a Vec if we're going
    to immediately iterate over it.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,15 @@ use std::fs;
 use ds18b20_parser::read_temp;
 
 fn main() {
-    let readings: Vec::<(f32, String)>;
-    match fs::read_dir("/sys/bus/w1/devices") {
-        Ok(d) => { 
-            readings = d.map(|e| e.unwrap().file_name())
-                .filter(|dev| dev != "w1_bus_master1")
-                .map(|dev| (read_temp(&dev).unwrap(), dev.into_string().unwrap()))
-                .collect::<Vec<_>>();
-
-            readings.iter().for_each(|r| println!("{} from {}", r.0, r.1));
-        },
-        Err(e) => { eprintln!("Could not list /sys/bus/w1/devices: {}", e); }
+    loop {
+        match fs::read_dir("/sys/bus/w1/devices") {
+            Ok(d) => { 
+                d.map(|e| e.unwrap().file_name())
+                    .filter(|dev| dev != "w1_bus_master1")
+                    .map(|dev| (read_temp(&dev).unwrap(), dev.into_string().unwrap()))
+                    .for_each(|r| println!("{} from {}", r.0, r.1));
+            },
+            Err(e) => { eprintln!("Could not list /sys/bus/w1/devices: {}", e); }
+        }
     }
 }
