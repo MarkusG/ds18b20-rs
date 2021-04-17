0cf4c77a28a897c2f35a1bdb2eaa60406be20caf
Mark Gross <mark.gross2001@gmail.com>
2021-04-17 16:12:37 -0700

    Support delay between measurements
    I don't exactly need per-second resolution for this project, and taking
    a reading every second would likely take up too much space over time,  so it's
    smarter to allow an arbitrary delay between readings.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-use std::fs;
+extern crate getopts;
 
+use std::{env, fs, thread, time};
+
+use getopts::Options;
 use postgres::{Client, NoTls};
 
 use temperature_monitor::{read_temp, record_temp};
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut opts = Options::new();
+    opts.optopt("d", "delay", "set delay between measurements", "DELAY");
+    let matches = opts.parse(&args[1..]).unwrap();
+    
+    let mut delay: u64 = 1000;
+    if let Some(val) = matches.opt_str("d") {
+        delay = val.parse::<u64>().unwrap_or(1000);
+    }
+
+
     let mut client = Client::connect("host=/var/run/postgresql/ user=pi dbname=temperature", NoTls).unwrap();
 
     loop {
@@ -17,5 +31,6 @@ fn main() {
             },
             Err(e) => { eprintln!("Could not list /sys/bus/w1/devices: {}", e); }
         }
+        thread::sleep(time::Duration::from_millis(delay));
     }
 }
